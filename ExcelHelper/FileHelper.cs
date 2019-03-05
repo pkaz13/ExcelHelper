ce82e50fe4c1af0b6a0ec437204fdcd726f4f306
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2019-03-05 18:59:49 +0100

    added project to repo

---
+++ ExcelHelper/FileHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelHelper
+{
+    static class FileHelper
+    {
+        public static FileInfo DeleteFileIfExist(FileInfo newFile)
+        {
+            if (newFile.Exists)
+            {
+                newFile.Delete();
+                newFile = new FileInfo(newFile.FullName);
+            }
+            return newFile;
+        }
+
+        public static string FixFileNameExcel(string fileName)
+        {
+            if (Path.HasExtension(fileName))
+            {
+                string extension = Path.GetExtension(fileName);
+                if (extension != ".xlsx")
+                    fileName = Path.GetFileNameWithoutExtension(fileName) + ".xlsx";
+            }
+            else
+                fileName += ".xlsx";
+
+            return fileName;
+        }
+    }
+}
